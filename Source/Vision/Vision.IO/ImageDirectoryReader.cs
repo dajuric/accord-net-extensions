29ed85144b9c9912517fb62fcefdf12c1a119821
dajuric <darko.juric2@gmail.com>
2014-02-19 00:11:29 +0100

    Commenting Video.IO

--- Source/Vision/Vision.IO/ImageDirectoryReader.cs
+++ Source/Vision/Vision.IO/ImageDirectoryReader.cs
@@ -7,6 +7,9 @@ using System.Threading;
 
 namespace Accord.Extensions.Vision
 {
+    /// <summary>
+    /// Provides a streamable view of an image directory.
+    /// </summary>
     public class ImageDirectoryReader : StreamableSource
     {
         string[] fileNames = null;
@@ -16,19 +19,24 @@ namespace Accord.Extensions.Vision
 
         #region Initialization
 
-        public ImageDirectoryReader(string filePath, string extension, bool useNaturalSorting = true)
-            : this(filePath, extension, useNaturalSorting, (x) => cvLoader(x))
-        {}
-
-        public ImageDirectoryReader(string filePath, string extension, bool useNaturalSorting, Func<string, IImage> loader)
+        /// <summary>
+        /// Creates an instance of <see cref="ImageDirectoryReader"/>.
+        /// </summary>
+        /// <param name="dirPath">The directory path.</param>
+        /// <param name="extension">The image extension.</param>
+        /// <param name="useNaturalSorting">Use natural sorting, otherwise raw image order is used.</param>
+        /// <param name="loader">Loader image function. If null default loader is used.</param>
+        public ImageDirectoryReader(string dirPath, string extension, bool useNaturalSorting, Func<string, IImage> loader = null)
         {
+            loader = loader ?? cvLoader;
+            
             this.IsLiveStream = false;
             this.CanSeek = true;
 
             this.loader = loader;
 
             string ext = "*." + extension.TrimStart('.', '*');
-            DirectoryInfo directoryInfo = new DirectoryInfo(filePath);
+            DirectoryInfo directoryInfo = new DirectoryInfo(dirPath);
 
             IEnumerable<string> files = null;
 
@@ -61,8 +69,14 @@ namespace Accord.Extensions.Vision
 
         #endregion
 
+        /// <summary>
+        /// Open the current stream. This overload does not do anything.
+        /// </summary>
         public override void Open() { }
 
+        /// <summary>
+        /// Closes the current stream. This overload resets position to zero.
+        /// </summary>
         public override void Close()
         {
             currentFrame = 0;
@@ -85,16 +99,31 @@ namespace Accord.Extensions.Vision
             return true;
         }
 
+        /// <summary>
+        /// Gets the total number of files in the specified directory which match the specified search criteria.
+        /// </summary>
         public override long Length { get { return fileNames.Length; } }
 
+        /// <summary>
+        /// Gets the current position within the stream.
+        /// </summary>
         public override long Position { get { return currentFrame; } }
 
+        /// <summary>
+        /// Sets the position within the current stream.
+        /// </summary>
+        /// <param name="offset">A frame index offset relative to the origin parameter.</param>
+        /// <param name="origin">A value of type System.IO.SeekOrigin indicating the reference point used to obtain the new position.</param>
+        /// <returns>The new position within the current stream.</returns>
         public override long Seek(long offset, SeekOrigin origin = SeekOrigin.Current)
         {
             this.currentFrame = base.Seek(offset, origin);
-            return currentFrame;
+            return Math.Max(0, Math.Min(currentFrame, this.Length));
         }
 
+        /// <summary>
+        /// Gets the current image file name.
+        /// </summary>
         public string CurrentImageName
         {
             get { return fileNames[this.Position]; }
