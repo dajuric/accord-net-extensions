e56ee6c8fbf3db26dbf321104fb106fa2a465e7b
dajuric <darko.juric2@gmail.com>
2014-08-02 04:22:09 +0200

    Adding licence; fixing minor bugs

--- Source/Imaging/FastTemplateMatching/FeatureExtraction/Template/ImageTemplate/ImageTemplate.cs
+++ Source/Imaging/FastTemplateMatching/FeatureExtraction/Template/ImageTemplate/ImageTemplate.cs
@@ -1,4 +1,26 @@
-﻿using System;
+#region Licence and Terms
+// Accord.NET Extensions Framework
+// https://github.com/dajuric/accord-net-extensions
+//
+// Copyright © Darko Jurić, 2014 
+// darko.juric2@gmail.com
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU Lesser General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU Lesser General Public License for more details.
+// 
+//   You should have received a copy of the GNU Lesser General Public License
+//   along with this program.  If not, see <https://www.gnu.org/licenses/lgpl.txt>.
+//
+#endregion
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Xml;
--- Source/Imaging/Imaging.GenericImage/ColorTypeConversions/ColorDepthConverter.cs
+++ Source/Imaging/Imaging.GenericImage/ColorTypeConversions/ColorDepthConverter.cs
@@ -1,4 +1,26 @@
-﻿using System;
+#region Licence and Terms
+// Accord.NET Extensions Framework
+// https://github.com/dajuric/accord-net-extensions
+//
+// Copyright © Darko Jurić, 2014 
+// darko.juric2@gmail.com
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU Lesser General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU Lesser General Public License for more details.
+// 
+//   You should have received a copy of the GNU Lesser General Public License
+//   along with this program.  If not, see <https://www.gnu.org/licenses/lgpl.txt>.
+//
+#endregion
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Accord.Extensions.Math.Geometry;
@@ -444,9 +466,9 @@ namespace Accord.Extensions.Imaging.Converters
                   () => conversionData.CreateFunc(img, conversionData.Destination),
                   (src, dest, area) => conversionData.ConvertFunc(src.GetSubRect(area), dest.GetSubRect(area))
 #if DEBUG
-                  ,new ParallelOptions { ForceSequential = true }
+                  ,new ParallelOptions2D { ForceSequential = true }
 #else
-, new ParallelOptions2D { ForceSequential = conversionData.ForceSequential }
+                  ,new ParallelOptions2D { ForceSequential = conversionData.ForceSequential }
 #endif
 );
 
